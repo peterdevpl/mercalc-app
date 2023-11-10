31c425e508e156d38105b8094c6d663708b56fad
Piotr Horzycki <piotr.horzycki@gmail.com>
2023-11-11 00:35:36 +0100

    Added sidebar

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import Nav from 'react-bootstrap/Nav';
+import NavItem from 'react-bootstrap/NavItem';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+
+export default function Sidebar() {
+  const pathname = usePathname();
+
+  return (
+    <>
+      <h3>mercalc</h3>
+      <Nav className="flex-column" as="nav">
+        <NavItem>
+          <Link href="/" className={`link ${pathname === '/' ? 'active' : ''}`}>Przegląd</Link>
+        </NavItem>
+        <NavItem>
+          <Link href="/orders" className={`link ${pathname === '/orders' ? 'active' : ''}`}>Zamówienia</Link>
+        </NavItem>
+        <NavItem>
+          <Link href="/stats" className={`link ${pathname === '/stats' ? 'active' : ''}`}>Statystyki</Link>
+        </NavItem>
+        <NavItem>
+          <Link href="/invoices" className={`link ${pathname === '/invoices' ? 'active' : ''}`}>Faktury</Link>
+        </NavItem>
+      </Nav>
+    </>
+  )
+}
