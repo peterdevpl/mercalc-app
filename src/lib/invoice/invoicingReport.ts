6a9f3dbb4f85d9eca0be7f6f76683033a7f8db99
Piotr Horzycki <piotr.horzycki@gmail.com>
2024-07-31 22:23:48 +0200

    Build invoices in the invoicing report generation (might be refactored later)

--- src/lib/invoice/invoicingReport.ts
+++ src/lib/invoice/invoicingReport.ts
@@ -1,9 +1,10 @@
-import { DateTime } from 'luxon';
+import buildInvoice from '@/lib/invoice/invoiceBuilder';
 import Decimal from 'decimal.js';
+import { CompanyData } from '@/lib/invoice/companyData';
 import { IOrder } from '@/lib/orderList';
 import { InvoicingReport } from '@/lib/invoice/invoices';
 
-export default function buildInvoicingReport(orders: IOrder[], monthYear: string, prefix: string, start: number, suffix: string): InvoicingReport {
+export default function buildInvoicingReport(orders: IOrder[], monthYear: string, issuer: CompanyData, prefix: string, start: number, suffix: string): InvoicingReport {
   const zero = new Decimal(0);
   const report: InvoicingReport = {
     rows: [],
@@ -17,14 +18,24 @@ export default function buildInvoicingReport(orders: IOrder[], monthYear: string
     if (orders[i].date.substring(0, 7) !== monthYear) {
       continue;
     }
+
+    const number = prefix + invoiceNumber.toString() + suffix;
+    const invoice = buildInvoice(orders[i], 'Faktura', number, issuer);  // todo: take type from input
+
     report.rows.push({
       rowId,
-      invoiceNumber: prefix + invoiceNumber.toString() + suffix,
-      date: DateTime.fromISO(orders[i].date),
-      totalEur: orders[i].total,
-      totalPln: orders[i].totalConverted,
-      exchangeRate: orders[i].rate,
-      country: orders[i].country,
+      orderId: invoice.orderId,
+      invoiceType: invoice.invoiceType,
+      invoiceNumber: invoice.invoiceNumber,
+      date: invoice.date,
+      buyerName: invoice.buyer.name,
+      totalNetEur: invoice.totalNetEur,
+      totalVatEur: invoice.totalVatEur,
+      totalEur: invoice.totalEur,
+      totalPln: invoice.totalConverted,
+      exchangeRate: invoice.exchangeRate,
+      country: invoice.country,
+      invoice
     });
     report.totalEur = report.totalEur.add(orders[i].total);
     report.totalPln = report.totalPln.add(orders[i].totalConverted ?? zero);
