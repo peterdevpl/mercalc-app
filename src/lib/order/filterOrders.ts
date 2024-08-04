0eecaf062d481a979dc90109a8c37debbca5fdd2
Piotr Horzycki <piotr.horzycki@gmail.com>
2024-08-04 20:18:25 +0200

    Implemented centralized filtering of orders

--- src/lib/order/filterOrders.ts
+++ src/lib/order/filterOrders.ts
@@ -11,15 +11,37 @@ const ossLimit = new Decimal('10000.00');
 
 export default function filterOrders(orders: IOrder[], filter: OrdersFilter): IOrder[] {
 	const result: IOrder[] = [];
+	let totalWithinEU = new Decimal(0);
+	let isWithinEU = false;
 
 	for (let i = 0; i < orders.length; i++) {
+		isWithinEU = orders[i].country !== thisCountry && euCountries.includes(orders[i].country);
+		if (isWithinEU && (filter.hasEUBelowOSS || filter.hasEUAboveOSS)) {
+			totalWithinEU = totalWithinEU.add(orders[i].total);
+		}
+
 		if (orders[i].date.substring(0, 7) !== filter.monthYear) {
 			continue;
 		}
 
-		// todo: implement other filters
+		if (filter.hasDomestic && orders[i].country === thisCountry) {
+			result.push(orders[i]);
+			continue;
+		}
 
-		result.push(orders[i]);
+		if (filter.hasEUBelowOSS && isWithinEU && totalWithinEU.lessThanOrEqualTo(ossLimit)) {
+			result.push(orders[i]);
+			continue;
+		}
+
+		if (filter.hasEUAboveOSS && isWithinEU && totalWithinEU.greaterThan(ossLimit)) {
+			result.push(orders[i]);
+			continue;
+		}
+
+		if (filter.hasOutsideEU && !euCountries.includes(orders[i].country)) {
+			result.push(orders[i]);
+		}
 	}
 
 	return result;
